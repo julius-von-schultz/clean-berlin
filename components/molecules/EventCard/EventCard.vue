b2869a2f0699d148370eb59dee48032ca386ade1
julius.schultz <julius.schultz@diva-e.com>
2025-01-05 17:43:42 +0100

    add short description to event card

--- components/molecules/EventCard/EventCard.vue
+++ components/molecules/EventCard/EventCard.vue
@@ -6,6 +6,7 @@
       class="event-card__image"
       src="https://hsg-berlin.de/fileadmin/_processed_/a/b/csm_Kehrenbu__rger_2_9f3039378c.png"
     />
+    <p class="event-card__short-description">Parkevent um zusammenzukommen und den Böckler-Park vom Müll zu befreien Parkevent um zusammenzukommen und den Böckler-Park vom Müll zu</p>
     <div class="event-card__text-wrapper">
       <div class="event-card__title">Park aufräumen</div>
       <div class="event-card__further-information-wrapper">
@@ -32,11 +33,30 @@
   @apply rounded-xl;
   @apply h-[320px];
   @apply w-[290px];
+  position: relative;
 
   &__image {
     @apply h-[180px];
     @apply rounded-t-xl;
     object-fit: cover;
+  }
+
+  &__short-description {
+    position: absolute;
+    top: 38.2%;
+    @apply px-6;
+    @apply text-cb-grey-11;
+    @apply font-semibold;
+    @apply max-h-[60px];
+    @apply text-xs;
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    background-color: #D6D6D699;
+    border-top: #d9d9d900 14px solid;
+    border-bottom: #d9d9d900 12px solid;
+    mask-image: linear-gradient(to bottom, transparent 0%, #D9D9D9 10%);
   }
 
   &__text-wrapper {
@@ -47,7 +67,7 @@
   }
 
   &__title {
-    @apply text-lg font-semibold;
+    @apply text-xl font-semibold;
     @apply pb-4;
   }
 
@@ -64,7 +84,7 @@
     width: 76px;
     @apply bg-cb-green-darker;
     bottom: 320px;
-    left: 160px;
+    left: 182px;
     font-size: 10px;
     justify-content: center;
     @apply flex;
@@ -74,5 +94,3 @@
   }
 }
 </style>
-<script setup lang="ts">
-</script>
